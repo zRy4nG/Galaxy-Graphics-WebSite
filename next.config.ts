6249fb231ec8e883e7af4b33d6a080796ebde995
zRy4nG <135639991+zRy4nG@users.noreply.github.com>
2025-07-31 14:03:36 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['i.imgur.com', 'cdn.discordapp.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
